d8a73ba865ba591e3f92f575d61b4d38b99cd5ac
Leandro Buzzi <77006961+likahon@users.noreply.github.com>
2021-09-20 14:27:34 -0300

    Se corrige entrega

--- JavaScript/00 - Proyecto/js/entregableArraysProyectoPersonal.js
+++ JavaScript/00 - Proyecto/js/entregableArraysProyectoPersonal.js
@@ -1,18 +1,16 @@
-    {id:12, producto: "Asrock A320M/HDV", precio: 5970, categoria: "motherboard"}, 
-    {id:13, producto: "Asrock B550 PG", precio: 27250, categoria: "motherboard"}, 
 const arrayProductos = [
-    {id:1, marca: "AMD" ,modelo: "Athlon 3000G", precio: 18500, categoria: "procesadores"}, 
-    {id:2, marca: "AMD" ,modelo: "A12 9800E", precio: 19500, categoria: "procesadores"},
-    {id:3, marca: "AMD" ,modelo: "Ryzen 3 3200G", precio: 32000, categoria: "procesadores"}, 
-    {id:4, marca: "AMD" ,modelo: "Ryzen 5 5600X", precio: 36800, categoria: "procesadores"}, 
-    {id:5, marca: "AMD" ,modelo: "Ryzen 7 5800G", precio: 53000, categoria: "procesadores"}, 
-    {id:6, marca: "AMD" ,modelo: "Ryzen 9 5950X", precio: 111920, categoria: "procesadores"},
-    {id:6, marca: "Intel" ,modelo: "Pentium Gold G6400", precio: 10290, categoria: "procesadores"}, 
-    {id:7, marca: "Intel" ,modelo: "Pentium Gold G6405", precio: 10300, categoria: "procesadores"}, 
-    {id:8, marca: "Intel" ,modelo: "i3 9100", precio: 22100, categoria: "procesadores"}, 
-    {id:9, marca: "Intel" ,modelo: "i5 11600K", precio: 43500, categoria: "procesadores"}, 
-    {id:10, marca: "Intel" ,modelo: "i7 11700KF", precio: 68570, categoria: "procesadores"}, 
-    {id:11, marca: "Intel" ,modelo: "i9 11900K", precio: 85500, categoria: "procesadores"},
+    {id:1, marca: "AMD" ,modelo: "Athlon 3000G", precio: 18500, categoria: "procesador"}, 
+    {id:2, marca: "AMD" ,modelo: "A12 9800E", precio: 19500, categoria: "procesador"},
+    {id:3, marca: "AMD" ,modelo: "Ryzen 3 3200G", precio: 32000, categoria: "procesador"}, 
+    {id:4, marca: "AMD" ,modelo: "Ryzen 5 5600X", precio: 36800, categoria: "procesador"}, 
+    {id:5, marca: "AMD" ,modelo: "Ryzen 7 5800G", precio: 53000, categoria: "procesador"}, 
+    {id:6, marca: "AMD" ,modelo: "Ryzen 9 5950X", precio: 111920, categoria: "procesador"},
+    {id:6, marca: "Intel" ,modelo: "Pentium Gold G6400", precio: 10290, categoria: "procesador"}, 
+    {id:7, marca: "Intel" ,modelo: "Pentium Gold G6405", precio: 10300, categoria: "procesador"}, 
+    {id:8, marca: "Intel" ,modelo: "i3 9100", precio: 22100, categoria: "procesador"}, 
+    {id:9, marca: "Intel" ,modelo: "i5 11600K", precio: 43500, categoria: "procesador"}, 
+    {id:10, marca: "Intel" ,modelo: "i7 11700KF", precio: 68570, categoria: "procesador"}, 
+    {id:11, marca: "Intel" ,modelo: "i9 11900K", precio: 85500, categoria: "procesador"},
     {id:12, marca: "Asrock" ,modelo: "Asrock A320M/HDV", precio: 5970, categoria: "motherboard"}, 
     {id:13, marca: "Asrock" ,modelo: "Asrock B550 PG", precio: 27250, categoria: "motherboard"}, 
     {id:14, marca: "Asus" ,modelo: "ASUS TUF X570", precio: 27400, categoria: "motherboard"}, 
@@ -27,27 +25,18 @@ const arrayProductos = [
     {id:23, marca: "Gigabyte" ,modelo: "Gigabyte Z590 Aorus Xtreme", precio: 120000, categoria: "motherboard"}
 ];
 
+let arrayCategoria = [];
+
+let categoria = prompt(`
+        Ingrese el nombre de la categoría:
+        1. Procesador
+        2. Motherboard
+`).toLowerCase();
+
+for ( let i=0; i < arrayProductos.length; i++) {
+    if (arrayProductos[i].categoria == categoria) {
+        arrayCategoria.push(arrayProductos[i]);
+}}
 
 
-let categoria = parseInt(prompt(`
-        Seleccione una categoría:
-        1. Procesadores
-        2. Motherboards
-`));
-
-switch (categoria) {
-    case 1:
-        for(const categoriaProduct of arrayProductos){
-            console.log(categoriaProduct.modelo);
-        }
-        break;
-    case 2:
-        for(const categoriaProduct of arrayProductos){
-            console.log(categoriaProduct.modelo);
-        }
-        break;
-
-    default:
-        console.log("No seleccionó una categoría válida.")
-        break;
-}
+console.log(arrayCategoria);
