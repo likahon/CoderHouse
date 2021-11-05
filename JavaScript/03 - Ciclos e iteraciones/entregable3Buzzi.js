79ac6b8fa44ba300291e41de7dcd9be9f86af13e
Leandro Buzzi <77006961+likahon@users.noreply.github.com>
2021-11-04 22:58:09 -0300

    correcciones

--- JavaScript/03 - Ciclos e iteraciones/entregable3Buzzi.js
+++ JavaScript/03 - Ciclos e iteraciones/entregable3Buzzi.js
@@ -1,18 +1,38 @@
+// 1 - Pedir número mediante prompt y sumarle otro número en cada repetición,realizando una salida por cada resultado
+
 /* let numero = parseInt(prompt("Ingrese un numero"));
 let resultado = 0;
 
-while (numero != "ESC") {
-
-    console.log(numero);
+while (numero != 0) {
+    
     numero = parseInt(prompt("Ingrese un numero"));
-}
- */
+    resultado = resultado + numero;
+    console.log(resultado);
 
-let usuario = prompt("Ingrese un numero");
+} */
 
 
-while (usuario != "ESC") {
+// 2 - Pedir un texto mediante prompt, concatenar un valor en cada repetición, realizando una salida por cada resultado, hasta que se ingresa “ESC”.
 
-    console.log(usuario);
-    numero = usuario = prompt("Ingrese un numero");
-}
+
+/* let texto = prompt("Ingrese un texto");
+let cadena = "";
+
+while (texto != "ESC") {
+    
+    texto = prompt("Ingrese un texto");
+    cadena = cadena + texto;
+    console.log("" + cadena);
+
+} */
+
+// 3 - Pedir un número por prompt, repetir la salida del mensaje “Hola” la cantidad de veces ingresada.
+
+/* let saludo = parseInt(prompt("Ingrese la cantidad de veces que desea repetir la palabra HOLA"));
+i = 0;
+
+do{
+    i++;
+    console.log("HOLA");
+
+}while(i < saludo) */
