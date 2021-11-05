6df24303761003f3d2ecf1f20294bf7a75e6dcf6
Leandro Buzzi <77006961+likahon@users.noreply.github.com>
2021-11-04 22:21:12 -0300

    entregable

--- JavaScript/03 - Ciclos e iteraciones/entregable3Buzzi.js
+++ JavaScript/03 - Ciclos e iteraciones/entregable3Buzzi.js
@@ -1,9 +1,9 @@
-numero = parseInt(prompt("ingrese un número"));
+let numero = parseInt(prompt("ingrese un número"));
 let resultado = 0;
 
 while (numero != 0) {
     
-    let numero = parseInt(prompt("ingrese un número"));
+    numero = parseInt(prompt("ingrese un número"));
     resultado = resultado + numero;
     console.log(resultado);
 }
