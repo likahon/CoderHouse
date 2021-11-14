ce98705b8bf68413cc5f34616086bd3be0e0d2d4
Leandro Buzzi <77006961+likahon@users.noreply.github.com>
2021-11-13 21:30:51 -0300

    se suma objetos al proyecto

--- JavaScript/00 - Proyecto/js/proyectoBuzzi.js
+++ JavaScript/00 - Proyecto/js/proyectoBuzzi.js
@@ -1,46 +1,82 @@
+let interesesPorCuotas = {
+    una: 0.10,
+    tres: 0.30,
+    seis: 0.60,
+    doce: 2
+}
 
-function calcularIntereses(monto, cuotas){
+class registrarCliente{
+
+    constructor(nombreApellido, dni, edad, pais, provincia, localidad, direccion, codigoPostal, telefono, montoPrestamo, cantCuotas, precioCuotas){
     
-    let intereses;
+        this.nombreApellido = nombreApellido;
+        this.dni = dni;
+        this.edad = edad;
+        this.pais = pais;
+        this.provincia = provincia;
+        this.localidad = localidad;
+        this.direccion = direccion;
+        this.codigoPostal = codigoPostal;
+        this.telefono = telefono;
+        this.montoPrestamo = montoPrestamo;
+        this.cantCuotas = cantCuotas;
+        this.precioCuotas = precioCuotas;
 
-    /* if(cuotas == 3){
-        intereses = monto * 0.10;
-        return intereses;
-    }else if(cuotas == 6){
-        intereses = monto * 0.30;
-        return intereses;
-    }else if(cuotas == 12) {
-        intereses = monto * 0.30;
-        return intereses;
-    } */
+    }
 
-    switch (cuotas) {
-        case 3:
-            intereses = monto * 0.10;
-            return intereses;
-            break;
+    agradecerCliente(){
 
-        case 6:
-            intereses = monto * 0.10;
-            return intereses;
-            break;
+        alert(`${this.nombre}, su crédito está aprobado, gracias por confiar en nosotros`);
 
-        case 6:
-            intereses = monto * 0.10;
-            return intereses;
-            break;
-    
-        default:
-            
-            break;
     }
 
 }
 
 
-let monto = parseInt(prompt("ingrese el monto deseado"));
-let cuotas = parseInt(prompt("ingrese la cantidad de cuotas: 3 / 6 / 12"));
+function calcularInteres(montoPrestamo, cantCuotas){
+    
+    let interes;
 
-let precioTotal = monto + calcularIntereses(monto, cuotas);
+        switch (cantCuotas) {
+            case 3:
+                interes = montoPrestamo * interesesPorCuotas.tres;
+                return interes;
 
-alert(precioTotal);
+            case 6:
+                interes = montoPrestamo * interesesPorCuotas.seis;
+                return interes;
+
+            case 12:
+                interes = montoPrestamo * interesesPorCuotas.doce;
+                return interes;
+        
+            default:
+                alert("Ingrese un montoPrestamo correcto");
+                break;
+        }
+
+
+}
+
+
+
+let montoPrestamo = parseInt(prompt("ingrese el monto que desea recibir"));
+let cantCuotas = parseInt(prompt("ingrese la cantidad de cantCuotas: 1 - 3 - 6 - 12"));
+
+let precioTotal = montoPrestamo + calcularInteres(montoPrestamo, cantCuotas);
+let precioCuotas = parseInt(precioTotal / cantCuotas);
+
+alert(`Debera abonar $${precioTotal} o $${precioCuotas} en ${cantCuotas}`);
+
+
+/* let nombreApellido = prompt("Ingrese su nombre y apellido");
+let dniCliente = parseInt(prompt("Ingrese su número de dni"));
+let edadCliente = parseInt(prompt("Ingrese su edad"));
+let paisCliente = prompt("Seleccione su país de residencia");
+let provinciaCliente = prompt("Seleccione su provincia de residencia");
+let localidadCliente = prompt("Seleccione su localidad de residencia");
+let direccionCliente = prompt("Seleccione la dirección de su domicilio");
+let codigoPostalCliente = parseInt(prompt("Ingrese su código postal"));
+let telefonoCliente = parseInt(prompt("Ingrese su número de teléfono"));
+
+let datosCliente = new registrarCliente(nombreApellido, edadCliente, paisCliente, provinciaCliente, localidadCliente, direccionCliente, codigoPostalCliente, telefonoCliente, precioTotal, cantCuotas, precioCuotas); */
