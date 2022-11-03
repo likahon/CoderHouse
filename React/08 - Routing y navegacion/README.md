1b3620d08c8449834ca6393aefd136b37c633abd
Leandro Buzzi <77006961+likahon@users.noreply.github.com>
2022-11-03 12:04:19 -0300

    clase 7

---
+++ React/08 - Routing y navegacion/README.md
@@ -0,0 +1,6 @@
+Con react no se pueden hacer rutas y se necesita una librería externa desde npm. (ej reach router)
+
+Librerias de rutas:
+React Router Dom
+Reach Router
+Wouter
---
+++ React/08 - Routing y navegacion/routing-navegacion/src/containers/Videos.jsx
@@ -0,0 +1,45 @@
+import Video from '../components/Video'
+import { useState, useEffect } from "react";
+import data from '../utils/data'
+import customFetch from '../utils/customFetch'
+
+
+const Videos = () =>{
+
+  const [datos, setDatos] = useState([])
+
+  const deleteVideo = (id) => {
+    let nuevosDatos = datos.filter(item => item.id != id) 
+    setDatos(nuevosDatos)
+  }
+  
+  useEffect(() => {
+
+    // componentDidMount
+    customFetch(2000, data)
+      .then(response => setDatos(response))
+      .catch(err => console.log(err))
+      
+  }, []);
+
+    return(
+
+        <>
+        {
+          datos.map(item => (
+            <Video
+              key={item.id}
+              title={item.title}
+              dateAdded={item.dateAdded}
+              channel={item.channel}
+              thumbnails={item.thumbnail}
+              description={item.description}
+              deleteVideo={() => deleteVideo(item.id)} />
+          ))
+        }
+        </>
+
+    );
+};
+
+export default Videos;
